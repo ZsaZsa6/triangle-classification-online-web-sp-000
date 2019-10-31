d76ca19a461ec827a319af365bac9437b6f4aedb
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-31 15:57:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
     end
 
     def kind
-      if ( l1 <= 0 || l2 <= 0 || l3 <= 0 )
+      if ( l1 <= 0 || l2 <= 0 || l3 <= 0 ) || ( l1 + l2 <= l3 ) || ( l1 + l3 <= l2 ) || ( l2 + l3 <= l1)
         raise TriangleError
       elsif ( l1 == l2 ) && ( l1 == l3 )
   	    return :equilateral
@@ -20,7 +20,8 @@ class Triangle
 
   class TriangleError < StandardError
     def message
-    puts "All sides of a triangle must be more than zero. The sum of the lengths of any two sides of a triangle always exceeds the length of the third side."
+    puts "All sides of a triangle must be more than zero.
+          The sum of the lengths of any two sides of a triangle always exceeds the length of the third side."
     end
   end
 end
