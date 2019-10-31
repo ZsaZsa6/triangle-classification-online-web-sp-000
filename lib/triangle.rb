d4b49657785cec8839ca8a8ae488587f374efe40
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-31 15:58:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
     def kind
       if ( l1 <= 0 || l2 <= 0 || l3 <= 0 ) || ( l1 + l2 <= l3 ) || ( l1 + l3 <= l2 ) || ( l2 + l3 <= l1)
         raise TriangleError
-      elsif ( l1 == l2 ) && ( l1 == l3 )
+      elsif ( l1 == l2 ) && ( l1 == l3 ) && ( l2 == l3 )
   	    return :equilateral
       elsif ( l1 == l3 ) && ( l2 == l3)
         return :isosceles
