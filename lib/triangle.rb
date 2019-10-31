83d04e83356752a19af4a6a71ddb6f2153b05a22
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-31 15:05:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,14 @@
 class Triangle
-  attr_accessor :length1, :length2, :length3
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+    attr_accessor :length1, :length2, :length3
+    def initialize(attributes)
+      attributes.each {|key, value| self.send(("#{key}="), value)}
+    end
+
+    def kind
+      
+    end
+
+  class TriangleError < StandardError
+    puts "The sum of the lengths of any two sides of a triangle always exceeds the length of the third side."
   end
-
-  def kind
-
-    
-    puts The sum of the lengths of any two sides of a triangle always exceeds the length of the third side."
 end
